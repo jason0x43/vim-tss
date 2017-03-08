4edddc63ee9fd719964280eb1d600b5c96d33c17
Jason Cheatham <j.cheatham@gmail.com>
2017-03-08 13:52:35 -0500

    Handle write sync EAGAIN failures

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -21,15 +21,30 @@ export function error(...args: any[]) {
 export function log(...args: any[]) {
 	const time = new Date().toLocaleTimeString('en-US', { hour12: false });
 	// Sync write to stderr
-	writeSync(2, `${time} ${format(args[0], ...args.slice(1))}\n`);
+	while (true) {
+		try {
+			writeSync(2, `${time} ${format(args[0], ...args.slice(1))}\n`);
+			break;
+		}
+		catch (error) {
+			// try again
+		}
+	}
 }
 
 export function print(message: Buffer | string) {
 	if (typeof message !== 'string') {
 		message = message.toString('utf8');
 	}
-	// Sync write to stdout
-	writeSync(1, message);
+	while (true) {
+		try {
+			writeSync(1, message);
+			break;
+		}
+		catch (error) {
+			// try again
+		}
+	}
 }
 
 let _debug = (..._args: any[]) => {};
